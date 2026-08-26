69311b25ba456907ed48dae9c5b53fba1d97ab42
Firat Sertgoz <f@nuff.tech>
2026-08-27 01:51:33 +0200

    fix(work-review): remove redundant escape

--- src/server/work-review/render.ts
+++ src/server/work-review/render.ts
@@ -13,7 +13,7 @@ function escapeHtml(value: string): string {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&#39;");
 }
 
